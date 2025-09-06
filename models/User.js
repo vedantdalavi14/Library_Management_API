918b45a899572aa3118033cd86c6bb53a197876a
vedantdalavi14 <vedantdalavi14@gmail.com>
2025-09-06 15:38:34 +0530

    Add JWT and bcrypt dependencies for authentication and user model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Please provide a username'],
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 6,
+    select: false // Don't send back the password in responses
+  },
+  role: {
+    type: String,
+    enum: ['Member', 'Admin'], // Role can only be one of these values
+    default: 'Member'
+  }
+});
+
+// Encrypt password before saving the user
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model('User', UserSchema);
